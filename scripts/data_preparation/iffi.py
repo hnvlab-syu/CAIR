4e9f073c856f59a1be8b1ca86dcfb505dab39a98
unacanal <woonhahaha@gmail.com>
2022-07-13 08:41:07 +0000

    Add validation data preparation code and modify description

--- scripts/data_preparation/iffi.py
+++ scripts/data_preparation/iffi.py
@@ -1,12 +1,36 @@
 '''
-Train 데이터를 모두 불러와서 필터 이미지는 input 폴더에 저장하고, Original 이미지는 target 폴더에 저장한다.
+  | - datasets
+  |  | - IFFI
+  |  |  | - IFFI-dataset-hr-train
+  |  |  |  | - 0
+  |  |  |  | - 1
+  |  |  |  | - 2
+  |  |  |  | - ...
+  |  |  | - IFFI-dataset-lr-train
+  |  |  |  | - 0
+  |  |  |  | - 1
+  |  |  |  | - 2
+  |  |  |  | - ...
+  |  |  | - IFFI-dataset-lr-valid
+  |  |  |  | - 0
+  |  |  |  | - 1
+  |  |  |  | - 2
+  |  |  |  | - ...
+
+ 1. Get all train data from 'datasets/IFFI/IFFI-dataset-hr-train'
+     and copy IG filtered images to 'train/input' folder,
+     and copy Original image to 'train/target' folder.
+ 2. Get all valid data
+     and copy to 'val/input' folder.
 '''
+
 import os
 from glob import glob
 import shutil
 
-basedir = '/workspace/why/codalab/NTIRE22/NAFNet/datasets/IFFI/'
+basedir = './datasets/IFFI/'
 
+# Train
 folders = ['IFFI-dataset-hr-train', 'IFFI-dataset-lr-train']
 
 for folder in folders:
@@ -19,3 +43,8 @@ for folder in folders:
             original_file = os.path.join(os.path.dirname(file), basename.split('_')[0] + '_Original.jpg')
             shutil.copy(original_file, os.path.join(basedir, 'train', 'target', hr_lr + '_' + os.path.basename(file)))
             shutil.copy(file, os.path.join(basedir, 'train', 'input', hr_lr + '_' + os.path.basename(file)))
+            
+# Validation            
+val_path_list = glob(os.path.join(basedir, 'IFFI-dataset-lr-valid', '*/*'), recursive=True)
+for file in val_path_list:
+    shutil.copy(file, os.path.join(basedir, 'val/input'))
