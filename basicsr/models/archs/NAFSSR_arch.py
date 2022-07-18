fd7615eb41bfbaed6a392793b514ad731c9acd27
unacanal <woonhahaha@gmail.com>
2022-07-18 08:39:55 +0000

    Add new arch 'NAFNetSR + LAM'

--- basicsr/models/archs/NAFSSR_arch.py
+++ basicsr/models/archs/NAFSSR_arch.py
@@ -128,6 +128,155 @@ class NAFNetSR(nn.Module):
         out = out + inp_hr
         return out
 
+class NAFNetSR(nn.Module):
+    '''
+    NAFNet for Super-Resolution
+    '''
+    def __init__(self, up_scale=4, width=48, num_blks=16, img_channel=3, drop_path_rate=0., drop_out_rate=0., fusion_from=-1, fusion_to=-1, dual=False):
+        super().__init__()
+        self.dual = dual    # dual input for stereo SR (left view, right view)
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.body = MySequential(
+            *[DropPath(
+                drop_path_rate, 
+                NAFBlockSR(
+                    width, 
+                    fusion=(fusion_from <= i and i <= fusion_to), 
+                    drop_out_rate=drop_out_rate
+                )) for i in range(num_blks)]
+        )
+
+        self.up = nn.Sequential(
+            nn.Conv2d(in_channels=width, out_channels=img_channel * up_scale**2, kernel_size=3, padding=1, stride=1, groups=1, bias=True),
+            nn.PixelShuffle(up_scale)
+        )
+        self.up_scale = up_scale
+
+    def forward(self, inp):
+        inp_hr = F.interpolate(inp, scale_factor=self.up_scale, mode='bilinear')
+        if self.dual:
+            inp = inp.chunk(2, dim=1)
+        else:
+            inp = (inp, )
+        feats = [self.intro(x) for x in inp]
+        feats = self.body(*feats)
+        out = torch.cat([self.up(x) for x in feats], dim=1)
+        out = out + inp_hr
+        return out
+
+class LAM_Module(nn.Module):
+    '''
+    Layer attention module from 
+    https://github.com/wwlCape/HAN/blob/master/src/model/han.py
+    '''
+    def __init__(self, in_dim):
+        super(LAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax  = nn.Softmax(dim=-1)
+        
+    def forward(self, x):
+        '''
+        inputs :
+            x : input feature maps( B X N X C X H X W)
+        returns :
+            out : attention value + input feature
+            attention: B X N X N
+        '''
+        m_batchsize, N, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, N, -1)
+        proj_key = x.view(m_batchsize, N, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = x.view(m_batchsize, N, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, N, C, height, width)
+
+        out = self.gamma * out + x
+        out = out.view(m_batchsize, -1, height, width)
+        return out
+    
+class NAFLSR(nn.Module):
+    '''
+    NAFNet + Layer attention for Super-Resolution
+    '''
+    def __init__(self, up_scale=4, width=48, num_blks=16, img_channel=3, drop_path_rate=0., drop_out_rate=0., fusion_from=-1, fusion_to=-1):
+        super().__init__()
+        self.intro = nn.Conv2d(
+            in_channels=img_channel,
+            out_channels=width,
+            kernel_size=3,
+            padding=1,
+            stride=1,
+            groups=1,
+            bias=True
+        )
+        self.body = MySequential(
+            *[DropPath(
+                drop_path_rate, 
+                NAFBlockSR(
+                    width, 
+                    fusion=(fusion_from <= i and i <= fusion_to), 
+                    drop_out_rate=drop_out_rate
+                )) for i in range(num_blks)]
+        )
+        
+        self.inp_up = nn.Sequential(
+            nn.Conv2d(
+                in_channels=img_channel,
+                out_channels=img_channel * up_scale**2,
+                kernel_size=3,
+                padding=1,
+                stride=1,
+                groups=1,
+                bias=True
+            ),
+            nn.PixelShuffle(up_scale)
+        )
+        self.out_up = nn.Sequential(
+            nn.Conv2d(
+                in_channels=width,
+                out_channels=img_channel * up_scale**2,
+                kernel_size=3,
+                padding=1, 
+                stride=1,
+                groups=1,
+                bias=True
+            ),
+            nn.PixelShuffle(up_scale)
+        )
+        
+        self.up_scale = up_scale
+        self.conv1 = nn.Conv2d(
+                        in_channels=num_blks * width,
+                        out_channels=width,
+                        kernel_size=1
+                    )
+        self.la = LAM_Module(width)
+        
+    def forward(self, inp):
+        inp_hr = self.inp_up(inp)
+        feats = self.intro(inp)
+        
+        for name, midlayer in self.body._modules.items():
+            feats = midlayer(feats)[0]
+            if name == '0':
+                feats_cat = feats.unsqueeze(1)
+            else:
+                feats_cat = torch.cat([feats.unsqueeze(1), feats_cat], 1)
+        nf_out = feats
+        lam_out = self.la(feats_cat)
+        lam_out = self.conv1(lam_out)
+        
+        out = nf_out + lam_out
+        out = self.out_up(out)
+        out = out + inp_hr
+        return out
+
 class NAFSSR(Local_Base, NAFNetSR):
     def __init__(self, *args, train_size=(1, 6, 30, 90), fast_imp=False, fusion_from=-1, fusion_to=1000, **kwargs):
         Local_Base.__init__(self)
