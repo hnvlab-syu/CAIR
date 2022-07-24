97da533ffff972a9d9ef3b49a213e0ad4117503d
unacanal <woonhahaha@gmail.com>
2022-07-25 01:26:31 +0900

    Add new arch: Color Attention NAFNet

--- basicsr/models/archs/NAFNet_arch.py
+++ basicsr/models/archs/NAFNet_arch.py
@@ -20,6 +20,7 @@ import torch.nn.functional as F
 from basicsr.models.archs.arch_util import MySequential
 from basicsr.models.archs.arch_util import LayerNorm2d
 from basicsr.models.archs.local_arch import Local_Base
+from basicsr.models.archs.arch_util import get_gaussian_kernel, Upsample
 
 class SimpleGate(nn.Module):
     def forward(self, x):
@@ -176,6 +177,167 @@ class NAFNetLocal(Local_Base, NAFNet):
         with torch.no_grad():
             self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
 
+
+class NAFGroup(nn.Module):
+    def __init__(self, n_feats, num_nb=2):
+        super().__init__()
+        self.body = nn.Sequential(
+                        *[NAFBlock(n_feats) for _ in range(num_nb)]
+                    )
+        self.tail = nn.Conv2d(in_channels=n_feats, out_channels=n_feats, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+
+    def forward(self, inp):
+        x = inp
+
+        x = self.body(x)
+        x = self.tail(x)
+        return x + inp
+
+
+class ColorAttention(nn.Module):
+    def __init__(self, in_channel, num_feat):
+        super(ColorAttention, self).__init__()        
+        output_nc = num_feat
+
+        num_ng = 2
+        num_nb = 2
+        kernel_size = 1
+
+        sigma = 12 ## GAUSSIAN_SIGMA
+
+        modules_head = [nn.Conv2d(in_channel, num_feat, kernel_size=kernel_size, padding=0, stride=1, groups=1, bias=True)]
+
+        modules_downsample = [nn.MaxPool2d(kernel_size=2)] 
+        self.downsample = nn.Sequential(*modules_downsample)
+
+        modules_body = [
+                NAFGroup(num_feat, num_nb) \
+                for _ in range(num_ng)
+            ]
+        
+        modules_body.append(nn.Conv2d(num_feat, num_feat, kernel_size, padding=0, stride=1, groups=1, bias=True))
+
+        modules_tail = [nn.Conv2d(num_feat, output_nc, kernel_size, padding=0, stride=1, groups=1, bias=True), nn.Sigmoid()]
+
+        self.head = nn.Sequential(*modules_head)
+        self.body = nn.Sequential(*modules_body)
+        self.tail = nn.Sequential(*modules_tail)
+        self.blur, self.pad = get_gaussian_kernel(sigma=sigma)
+
+    def forward(self, x):
+        x = F.pad(x, (self.pad, self.pad, self.pad, self.pad), mode='reflect')
+        x = self.blur(x)
+        x = self.head(x)
+        x = self.downsample(x)  
+        x = self.body(x)
+        x = self.tail(x)
+        return x
+
+
+class ColorNAFNet(nn.Module):
+
+    def __init__(self, img_channel=3, width=16, middle_blk_num=1, enc_blk_nums=[], dec_blk_nums=[]):
+        super().__init__()
+
+        self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True)
+
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+        self.middle_blks = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+
+        chan = width
+        for num in enc_blk_nums:
+            self.encoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+            self.downs.append(
+                nn.Conv2d(chan, 2*chan, 2, 2)
+            )
+            chan = chan * 2
+
+        self.middle_blks = \
+            nn.Sequential(
+                *[NAFBlock(chan) for _ in range(middle_blk_num)]
+            )
+
+        for num in dec_blk_nums:
+            self.ups.append(
+                nn.Sequential(
+                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.PixelShuffle(2)
+                )
+            )
+            chan = chan // 2
+            self.decoders.append(
+                nn.Sequential(
+                    *[NAFBlock(chan) for _ in range(num)]
+                )
+            )
+
+        self.padder_size = 2 ** len(self.encoders)
+
+        self.color_attention = ColorAttention(in_channel=img_channel, num_feat=width)
+        self.upsample = Upsample(scale=2, num_feat=width)
+        self.conv1x1 = nn.Conv2d(in_channels=width, out_channels=3, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+
+    def forward(self, inp):
+        B, C, H, W = inp.shape
+        inp = self.check_image_size(inp)
+
+        x = self.intro(inp)
+
+        encs = []
+
+        for encoder, down in zip(self.encoders, self.downs):
+            x = encoder(x)
+            encs.append(x)
+            x = down(x)
+
+        x = self.middle_blks(x)
+
+        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
+            x = up(x)
+            x = x + enc_skip
+            x = decoder(x)
+
+        x = self.ending(x)
+        x = x + inp
+        
+        color = self.color_attention(inp)
+        color = self.upsample(color)
+        color = self.conv1x1(color)
+        x = x + color
+
+        return x[:, :, :H, :W]
+
+    def check_image_size(self, x):
+        _, _, h, w = x.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
+        return x
+
+
+class ColorNAFNetLocal(Local_Base, ColorNAFNet):
+    def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
+        Local_Base.__init__(self)
+        ColorNAFNet.__init__(self, *args, **kwargs)
+
+        N, C, H, W = train_size
+        base_size = (int(H * 1.5), int(W * 1.5))
+
+        self.eval()
+        with torch.no_grad():
+            self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
+
+
 class DropPath(nn.Module):
     def __init__(self, drop_rate, module):
         super().__init__()
@@ -193,6 +355,7 @@ class DropPath(nn.Module):
             new_feats = tuple([x+factor*(new_x-x) for x, new_x in zip(feats, new_feats)])
         return new_feats
     
+
 class LAM_Module(nn.Module):
     '''
     Layer attention module from 
