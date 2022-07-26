29b980f4655eeac72a2630b24979c3001f38ec94
WHY <woonhahaha@gmail.com>
2022-07-26 21:31:22 +0900

    Revert "ColorMISONAFNet"

--- basicsr/models/archs/NAFNet_arch.py
+++ basicsr/models/archs/NAFNet_arch.py
@@ -327,155 +327,10 @@ class ColorNAFNet(nn.Module):
         return x
 
 
-class ColorMISONet(nn.Module):
-
-    def __init__(self, img_channel=3, width=16, middle_blk_num=1, enc_blk_nums=[], dec_blk_nums=[]):
-        super().__init__()
-
-        self.intro1 = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.intro2 = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.intro3 = nn.Conv2d(in_channels=img_channel, out_channels=width*2, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.intro4 = nn.Conv2d(in_channels=img_channel, out_channels=width*4, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.ending = nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-
-        self.encoders = nn.ModuleList()
-        self.decoders = nn.ModuleList()
-        self.middle_blks = nn.ModuleList()
-        self.ups = nn.ModuleList()
-        self.downs = nn.ModuleList()
-
-
-        chan = width
-        for num in enc_blk_nums:
-            self.encoders.append(
-                nn.Sequential(
-                    *[NAFBlock(chan) for _ in range(num)]
-                )
-            )
-            self.downs.append(
-                nn.Conv2d(chan, chan, 2, 2)
-            )
-            chan = chan * 2
-        
-        chan = chan // 2
-        self.middle_blks = \
-            nn.Sequential(
-                *[NAFBlock(chan) for _ in range(middle_blk_num)],
-                nn.Conv2d(chan, chan * 2, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-            )
-        
-        chan = chan * 2
-        for num in dec_blk_nums:
-            self.ups.append(
-                nn.Sequential(
-                    nn.Conv2d(chan, chan * 2, 1, bias=False),
-                    nn.PixelShuffle(2)
-                )
-            )
-            chan = chan // 2
-            self.decoders.append(
-                nn.Sequential(
-                    *[NAFBlock(chan) for _ in range(num)]
-                )
-            )
-            
-
-        self.padder_size = 2 ** len(self.encoders)
-
-        self.color_attention1 = ColorAttention(in_channel=img_channel, num_feat=width)
-        self.color_attention2 = ColorAttention(in_channel=img_channel, num_feat=width)
-        self.color_attention3 = ColorAttention(in_channel=img_channel, num_feat=width*2)
-        self.color_attention4 = ColorAttention(in_channel=img_channel, num_feat=width*4)
-        self.upsample = Upsample(scale=2, num_feat=width)
-        self.conv1x1 = nn.Conv2d(in_channels=width, out_channels=3, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-
-    def forward(self, inp):
-        B, C, H, W = inp.shape
-        datas = []
-        inp = self.check_image_size(inp)
-        datas.append(self.intro1(inp))
-
-        inp_2_color = self.color_attention2(inp)
-        inp_2_resize = F.interpolate(inp, scale_factor=0.5, mode='bicubic')
-        inp_2_temp = self.intro2(inp_2_resize)
-        inp_2_color = inp_2_temp * inp_2_color
-        inp_2_color = inp_2_temp + inp_2_color
-        datas.append(inp_2_color)
-
-        inp_4_color = self.color_attention3(inp_2_resize)
-        inp_4_resize = F.interpolate(inp_2_resize, scale_factor=0.5, mode='bicubic')
-        inp_4_temp = self.intro3(inp_4_resize)
-        inp_4_color = inp_4_temp * inp_4_color
-        inp_4_color = inp_4_temp + inp_4_color
-        datas.append(inp_4_color)
-
-        inp_8_color = self.color_attention4(inp_4_resize)
-        inp_8_resize = F.interpolate(inp_4_resize, scale_factor=0.5, mode='bicubic')
-        inp_8_temp = self.intro4(inp_8_resize)
-        inp_8_color = inp_8_temp * inp_8_color
-        inp_8_color = inp_8_temp + inp_8_color
-        datas.append(inp_8_color)
-
-
-        encs = []
-        num = [0, 1, 2, 3]
-        for encoder, down, data, i in zip(self.encoders, self.downs, datas, num):
-            if i == 0:
-                x = data
-            else:
-                x = torch.cat((x, data), 1)
-            x = encoder(x)
-            encs.append(x)
-            x = down(x)
-
-        x = self.middle_blks(x)
-        for decoder, up, enc_skip in zip(self.decoders, self.ups, encs[::-1]):
-            x = up(x)
-            x = x + enc_skip
-            x = decoder(x)
-
-        x = self.ending(x)
-        
-        color = self.color_attention1(inp)
-        color = self.upsample(color)
-        color = self.conv1x1(color)
-        color = x * color
-        x = x + color
-
-        x = x + inp
-
-        return x[:, :, :H, :W]
-
-    def check_image_size(self, x):
-        _, _, h, w = x.size()
-        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
-        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
-        x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
-        return x
-
-
 class ColorNAFNetLocal(Local_Base, ColorNAFNet):
     def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
         Local_Base.__init__(self)
         ColorNAFNet.__init__(self, *args, **kwargs)
-
-        N, C, H, W = train_size
-        base_size = (int(H * 1.5), int(W * 1.5))
-
-        self.eval()
-        with torch.no_grad():
-            self.convert(base_size=base_size, train_size=train_size, fast_imp=fast_imp)
-
-
-class ColorMISONetLocal(Local_Base, ColorMISONet):
-    def __init__(self, *args, train_size=(1, 3, 256, 256), fast_imp=False, **kwargs):
-        Local_Base.__init__(self)
-        ColorMISONet.__init__(self, *args, **kwargs)
 
         N, C, H, W = train_size
         base_size = (int(H * 1.5), int(W * 1.5))
@@ -611,7 +466,7 @@ class NAFL(nn.Module):
         lam_out = self.la(feats_cat)
         lam_out = self.conv1(lam_out)
         
-        out = nf_out + lam_outtorch.cat((x, data), 1)
+        out = nf_out + lam_out
         out = self.out_conv(out)
         out = out + inp_feat
         return out
